43154b4b57f4099dd68af812d803c3db1e76e65c
Williams Sissoko <wsissoko65@gmail.com>
2019-10-10 13:26:52 +0700

    Initial Commit

---
+++ FBGroup/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FBGroup
+//
+//  Created by Williams on 10/10/2562 BE.
+//  Copyright © 2562 Williams. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
